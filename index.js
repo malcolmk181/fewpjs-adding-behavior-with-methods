07ddb1054e3b8b10f85d1b285736123f7e365eed
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-29 09:54:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+class Pet {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+}
+
+class Cat extends Pet {
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog extends Pet {
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird extends Pet {
+    speak() {
+        if (this.sex === 'male') {
+            return `It's me! ${this.name}, the parrot!`;
+        } else {
+            return `${this.name} says squawk!`;
+        }
+    }
+}   
